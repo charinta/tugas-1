aadf8ccc5f19927596ce5bb80a5717238286791a
charinta <charintakantidewi@gmail.com>
2023-09-11 18:43:11 +0700

    tugas

--- main.dart
+++ main.dart
@@ -6,10 +6,12 @@ class Mahasiswa {
   Mahasiswa(this.nim, this.nama, this.tahunLahir);
 
   void perkenalan() {
-    print("Perkenalkan, nama saya $nama");
+    var usia = DateTime.now().year - tahunLahir;
+    print("Perkenalkan, nama saya $nama dengan nim $nim dan usia saya $usia tahun.");
   }
 }
 
 void main() {
-  var mhs = Mahasiswa("H1D020068", "Abdalhaqq Muhammad Saih", 2002);
+  Mahasiswa mhs = Mahasiswa("H1D021082", "Charinta Candrakanti Dewi", 2003);
+  mhs.perkenalan();
 }
